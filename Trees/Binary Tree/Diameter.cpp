7dd4964783e0815c37c903cdcdc64c3191f0da08
Aniket <63105008+iam-aniket@users.noreply.github.com>
2021-03-02 23:59:13 +0530

    Max (left ht + right ht) at every node

--- Trees/Binary Tree/Diameter.cpp
+++ Trees/Binary Tree/Diameter.cpp
@@ -1,3 +1,7 @@
+The question can be solved by small modification to program of Height of tree.
+The idea is quite simple. Max value of Height(leftSubtree)+Height(rightSubtree) (at any node ) is the diameter. Keep track of maxium diameter duing traversal and find the height of the tree.
+
+d=max(d,ld+rd); => This line maintains the max diameter.
 int diameterOfBinaryTree(TreeNode* root)
 {
 	int d = 0;
