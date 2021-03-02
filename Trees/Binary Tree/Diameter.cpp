11a0c269904e986d8ee454e26d435efd1eabdb37
Aniket <63105008+iam-aniket@users.noreply.github.com>
2021-03-02 23:59:52 +0530

    Max ld + rd

--- Trees/Binary Tree/Diameter.cpp
+++ Trees/Binary Tree/Diameter.cpp
@@ -1,7 +1,10 @@
-The question can be solved by small modification to program of Height of tree.
-The idea is quite simple. Max value of Height(leftSubtree)+Height(rightSubtree) (at any node ) is the diameter. Keep track of maxium diameter duing traversal and find the height of the tree.
+//The question can be solved by small modification to program of Height of tree.
 
-d=max(d,ld+rd); => This line maintains the max diameter.
+//The idea is quite simple. Max value of Height(leftSubtree)+Height(rightSubtree) (at any node ) is the diameter. 
+//Keep track of maxium diameter duing traversal and find the height of the tree.
+
+//d=max(d,ld+rd); => This line maintains the max diameter.
+	
 int diameterOfBinaryTree(TreeNode* root)
 {
 	int d = 0;
