a41dacdad6f7dc879b7f970ede89956f0fd2298a
Aniket <63105008+iam-aniket@users.noreply.github.com>
2021-03-05 23:51:12 +0530

    Like Vertical Traversal

---
+++ Trees/Traversals/Diagonal Traversal.cpp
@@ -0,0 +1,26 @@
+struct TreeNode
+{
+	int val;
+	TreeNode* left, right;
+}
+vector<int> diagonalTraversal(TreeNode * root)
+{
+	map<int, vector<int>> m;
+	vector<vector<int>> ans;
+
+	solve(root, ans, m);
+
+	for (auto x : m)
+		ans.pb(x);
+	return ans;
+}
+void solve(TreeNode * root, int depth, map<int, vector<int>>& m)
+{
+	if (root == NULL)
+		return;
+
+	m[depth].push_back(root->val);
+
+	solve(root->left, depth - 1, m);
+	solve(root->right, depth, m);
+}
