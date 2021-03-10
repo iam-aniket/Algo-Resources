3b4cc108fe25cbd3e7944acd711ab0eecf92e3e9
Aniket <63105008+iam-aniket@users.noreply.github.com>
2021-03-10 16:30:15 +0530

    Basics

--- Trees/BST/Insert in BST.cpp
+++ Trees/BST/Insert in BST.cpp
@@ -2,6 +2,12 @@ class Solution {
 public:
 	TreeNode* insertIntoBST(TreeNode *root, int val)
 	{
+		//return insertRecursive(root, val);
+		return insertIterative(root, val);
+	}
+	// TC = O(n) but SC also O(n) stack space
+	TreeNode* insertRecursive(TreeNode* root, int val)
+	{
 		if (root == NULL)
 		{
 			TreeNode *node = new TreeNode(val);
@@ -12,6 +18,34 @@ public:
 			root->left = insertIntoBST(root->left, val);
 		else
 			root->right = insertIntoBST(root->right, val);
+
+		return root;
+	}
+	// TC O(n) & SC O(1)
+	TreeNode* insertIterative(TreeNode* root, int val)
+	{
+		TreeNode* temp = root;
+		if (root == NULL)
+		{
+			TreeNode* node = new TreeNode(val);
+			return node;
+		}
+		//First find position to insert the node
+		TreeNode* pos = NULL;
+		while (temp != NULL)
+		{
+			pos = temp;//VVI - Since temp becomes NULL in next step hence storing is necessary
+			if (temp->val < val)
+				temp = temp->right;
+			else
+				temp = temp->left;
+		}
+
+		//Now just insert to pos left or right depending on val
+		if (pos->val < val)
+			pos->right = new TreeNode(val);
+		else
+			pos->left = new TreeNode(val);
 
 		return root;
 	}
