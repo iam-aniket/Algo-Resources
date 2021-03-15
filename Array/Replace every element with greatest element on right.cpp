0afcc59ede6a9679188f44509dac22cf751be83c
Aniket <63105008+iam-aniket@users.noreply.github.com>
2021-03-15 20:12:42 +0530

    Simple Suffix concept

---
+++ Array/Replace every element with greatest element on right.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+	vector<int> replaceElements(vector<int>& arr)
+	{
+		int n = arr.size(), m = -1;
+		for (int i = n - 1; i >= 0; i--)
+		{
+			int temp = arr[i];
+			arr[i] = m;
+			m = max(m, temp);
+		}
+		return arr;
+	}
+};
