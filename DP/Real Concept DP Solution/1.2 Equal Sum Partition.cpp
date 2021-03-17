0c9b0f30735f42f7aea07260fdc25499599ecb89
Aniket <63105008+iam-aniket@users.noreply.github.com>
2021-03-18 00:25:42 +0530

    Same as Subset sum with sum/2 if sum is even

--- DP/Real Concept DP Solution/1.2 Equal Sum Partition.cpp
+++ DP/Real Concept DP Solution/1.2 Equal Sum Partition.cpp
@@ -4,37 +4,48 @@
 
 //If sum of array is even
 //And if sum/2 is a possible subset sum -> return true
-int knapsackEqualSumPartition(vector<int> arr, int n)
+
+class Solution
 {
-	int sum = 0;
-	for (int i = 0; i < n; i++)
-		sum += arr[i];
+public:
 
-	if (sum % 2)
-		return false;
-	else //If Even Check if sum/2 is a possible subset sum
+	bool canPartition(vector<int>& arr)
 	{
-		//Check if half sum is possible
-		//Since the other half automatically will be equal(ie; sum/2)
-		sum = sum / 2;
+		int n = arr.size();
+		int sum = 0;
 
-		for (int i = 0; i < n + 1; i++)
-			dp[i][0] = true;
+		for (int i = 0; i < n; i++)
+			sum += arr[i];
 
-		for (int i = 1; i < sum + 1; i++)
-			dp[0][i] = false;
+		if (sum % 2)
+			return false;
 
-		for (int i = 1; i < n + 1; i++) //Note : i Started from 1
+		else //If Even Check if sum/2 is a possible subset sum
 		{
-			for (int j = 1; j < sum + 1; j++)
-			{
-				if (arr[i - 1] <= j)
-					dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
-				else
-					dp[i][j] = dp[i - 1][j];
-			}
-		}
+			bool dp[n + 1][sum + 1];
 
-		return dp[n][sum];
+			//Check if half sum is possible
+			//Since the other half automatically will be equal(ie; sum/2)
+			sum = sum / 2;
+
+			for (int i = 0; i < n + 1; i++)
+				dp[i][0] = true;
+
+			for (int i = 1; i < sum + 1; i++)
+				dp[0][i] = false;
+
+			for (int i = 1; i < n + 1; i++) //Note : i Started from 1
+			{
+				for (int j = 1; j < sum + 1; j++)
+				{
+					if (arr[i - 1] <= j)
+						dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
+					else
+						dp[i][j] = dp[i - 1][j];
+				}
+			}
+
+			return dp[n][sum];
+		}
 	}
-}
+};
