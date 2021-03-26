cd32b2905d08fe3348e63a2fc2f570de9c2acd5a
Aniket <63105008+iam-aniket@users.noreply.github.com>
2021-03-26 23:40:48 +0530

    Implementation nhi hua mere se

--- Array/Isomorphic String.cpp
+++ Array/Isomorphic String.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) 
     {
+        //256 is max ASCII value for the req range
         int m1[256] = {0},m2[256] = {0};
         for(int i = 0; i < s.size();i++)
         {
