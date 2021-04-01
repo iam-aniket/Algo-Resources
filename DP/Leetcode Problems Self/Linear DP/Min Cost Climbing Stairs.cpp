ef0811b643d048849b1d085df6c18e0e83d50f79
Aniket <63105008+iam-aniket@users.noreply.github.com>
2021-04-01 11:24:18 +0530

    Bad Desc

--- DP/Leetcode Problems Self/Linear DP/Min Cost Climbing Stairs.cpp
+++ DP/Leetcode Problems Self/Linear DP/Min Cost Climbing Stairs.cpp
@@ -2,6 +2,9 @@
 //Actually : 
 
 /*
+Input: cost = [10, 15, 20]
+Output: 15
+Explanation: Cheapest is start on cost[1], pay that cost and go to the top.
 
                            __________
                        ___| Final step
