4fd6abd3c61501ec45b7f58ebeb105662023c813
Aniket <63105008+iam-aniket@users.noreply.github.com>
2021-07-13 16:05:45 +0530

    Update Diagonal Traversal.cpp

--- Trees/Traversals/Diagonal Traversal.cpp
+++ Trees/Traversals/Diagonal Traversal.cpp
@@ -3,12 +3,13 @@ struct TreeNode
 	int val;
 	TreeNode* left, right;
 }
+vector<int> ans;
 vector<int> diagonalTraversal(TreeNode * root)
 {
 	map<int, vector<int>> m;
 	vector<vector<int>> ans;
 
-	solve(root, ans, m);
+	solve(root, 0, m);
 
 	for (auto x : m)
 		ans.pb(x);
@@ -21,6 +22,6 @@ void solve(TreeNode * root, int depth, map<int, vector<int>>& m)
 
 	m[depth].push_back(root->val);
 
-	solve(root->left, depth - 1, m);
+	solve(root->left, depth + 1, m);
 	solve(root->right, depth, m);
 }
